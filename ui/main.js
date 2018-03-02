e7ddacd1b119a8fc6cbcad7a8fb56c974dd6ec74
debomoy97 <36603269+debomoy97@users.noreply.github.com>
2018-03-02 23:00:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,6 @@
 console.log('Loaded!');
+var my = document.getElementById('my'
+    );
 var img = document.getElementById('pic'
     );
 var marginBottom = 0;
@@ -6,6 +8,6 @@ function moveRight(){
     marginBottom = marginBottom + 4;
     img.style.marginBottom = marginBottom + 'px';
 }
-img.onclick = function(){
+my.onclick = function(){
      var interval = setInterval(moveRight,50); 
     };
