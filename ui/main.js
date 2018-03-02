1c21e4d88afe0e22c393efe0de002b625a8e8853
debomoy97 <36603269+debomoy97@users.noreply.github.com>
2018-03-02 22:10:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element = document.getElementById('my'
     );
 element.innerHTML='Happy Birthday';
+var img = document.getElementById('pic'
+    );
+img.onclick = function(){
+     img.style.marginleft="500px"; 
+    }
